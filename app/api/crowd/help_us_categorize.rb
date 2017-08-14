a2502618533a2101534fa0c87771d55259a00fca
Alberto Pedron <pedron.albert@gmail.com>
2017-08-14 15:59:53 -0400

    Fix help_us_categorize endpoint

--- app/api/crowd/help_us_categorize.rb
+++ app/api/crowd/help_us_categorize.rb
@@ -4,14 +4,14 @@ class Crowd::HelpUsCategorize < Grape::API
       optional :titles, type: Array
     end
 
-    def find_answer(url)
-      HelpUsCategorizeAnswer.find_by(url: url)
+    def find_answer(title)
+      HelpUsCategorizeAnswer.find_by(title: title)
     end
   end
 
-  resource :help_us_categorize do
+  resource 'help-us-categorize' do
     post '/find' do
-      params.fetch(:title)
+      params.fetch(:titles)
             .map { |title| find_answer(title) }
             .reject(&:nil?)
     end
